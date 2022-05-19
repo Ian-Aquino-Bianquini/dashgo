bdfdcc04f2411cc71521f8102c8121217db90242
Ian de Aquino Bianquini <ian.aquino2003@gmail.com>
2022-05-19 11:15:01 -0300

    feat(SideBar): DrawerMenu Responsive

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,7 +1,42 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { SideBarNav } from "./SideBarNav";
 
 export function SideBar() {
+  const isDrawerSidebar = useBreakpointValue({
+    base: true,
+    lg: false,
+  });
+
+  if (isDrawerSidebar) {
+    return (
+      <Drawer
+        isOpen={true}
+        placement="left"
+        onOverlayClick={() => {}}
+        onClose={function () {}}
+      >
+        <DrawerOverlay>
+          <DrawerContent bg="gray.800" p="4">
+            <DrawerCloseButton mt="6" />
+            <DrawerHeader>Navegação</DrawerHeader>
+            <DrawerBody>
+              <SideBarNav />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    );
+  }
+
   return (
     <Box as="aside" w="64" mr="8">
       <SideBarNav />
