f0d6ac9db8bbf3c2c7d67566c02544436a7d654e
Ian de Aquino Bianquini <ian.aquino2003@gmail.com>
2022-05-20 13:32:29 -0300

    feat(Dashboard): style height Container

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -74,7 +74,7 @@ const series = [
 
 export default function Dashboard() {
   return (
-    <Flex direction="column" h="10vh">
+    <Flex direction="column" h="100vh">
       <Header />
 
       <Flex w="100%" my="6" maxW={1400} mx="auto" px="6">
